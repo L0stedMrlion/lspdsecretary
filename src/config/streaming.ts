717579dda58fbd1b1f4ca3d701f5fc36041f4674
Mrlion <87368344+L0stedMrlion@users.noreply.github.com>
2026-08-16 14:33:35 +0200

    Create streaming.ts

---
+++ src/config/streaming.ts
@@ -0,0 +1,7 @@
+export const STREAMER_ROLE_ID = '1287347387706118185';
+
+export const STREAM_URLS: Readonly<Record<string, string>> = {
+  '892296560845660212': 'https://kick.com/pentyyy',
+  '769892516152999957': 'https://www.twitch.tv/petrpetr0',
+  '710549603216261141': 'https://www.twitch.tv/lostedmrlion',
+};
